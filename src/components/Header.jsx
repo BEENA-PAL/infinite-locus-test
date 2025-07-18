f851fbb92497013e790ccfef6236fca718fa8e6a
beena pal <beenapal570@gmail.com>
2025-07-18 17:22:49 +0530

    replaced text logo with image logo

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -8,7 +8,10 @@ function Header() {
   return (
     <nav className="bg-white shadow-md px-6 py-4 flex justify-between items-center">
       <Link to="/" className="text-2xl font-bold text-green-600">
-        RecipeVerse
+        <img
+          src="https://cdn.zeptonow.com/web-static-assets-prod/artifacts/13.2.4/images/header/primary-logo.svg"
+          alt=""
+        />
       </Link>
       <div className="flex items-center gap-6">
         <Link to="/" className="hover:text-green-600">
